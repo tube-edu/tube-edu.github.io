ef551ee8960dd7c1ed3318b62c14712ec07cb061
Kokohachi <99192596+Kokohachi@users.noreply.github.com>
2023-12-26 01:22:01 +0900

    ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,46 @@ import "@mantine/core/styles.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
-
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
 import { createTheme, MantineProvider } from "@mantine/core";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
   return (
     <MantineProvider theme={theme}>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+            `,
+        }}
+      />
       <Component {...pageProps} />
       <SpeedInsights />
       <Analytics />
