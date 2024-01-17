2f36700423a5d31a82e375436fb4165fb7d40b6a
Kokohachi <99192596+Kokohachi@users.noreply.github.com>
2024-01-18 04:56:51 +0900

    go

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
   webpack: (config) => {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
-  }
+  },
 };
 
 module.exports = nextConfig;
--- pages/maintenance.tsx
+++ pages/maintenance.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/Header";
-
 export default function Maintanance() {
   return (
     <div>
